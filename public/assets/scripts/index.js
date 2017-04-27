20463185f8cfd15a28e0b1158b29a74d798fed1c
Michael Valdes <pandamayc_20@hotmail.com>
2017-04-27 18:20:44 -0500

    Cambios index.js

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		$('#'+pag).addClass("active");
 		$('#mdrop').addClass("active");
 	}
-	else if(pag==null)
+	else if(pag=="")
 	{
 		$('#home').addClass("active");
 	}
