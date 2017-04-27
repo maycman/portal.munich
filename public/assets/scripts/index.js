71289531cc59cc9547e7bcbd6c37b0e6fd76c0df
Michael Valdes <pandamayc_20@hotmail.com>
2017-04-27 18:01:42 -0500

    Definir Scripts

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	//Implementación de juego de clases
+	var pag = location.href.substring(location.href.lastIndexOf('/')+1, location.href.lastIndexOf(''));
+	alert("La ruta es: "+pag);
+});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,8 +9,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css">
 	<link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/6.0.0/normalize.css">
-	<link rel="stylesheet" type="text/css" href="../assets/content/index.css">
-	<link rel="stylesheet" type="text/css" href="../assets/content/trap.css">
+	<link rel="stylesheet" type="text/css" href="assets/content/index.css">
+	<link rel="stylesheet" type="text/css" href="assets/content/trap.css">
 </head>
 <body>
 	<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
@@ -52,4 +52,5 @@
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="assets/scripts/index.js" type="text/javascript"></script>
 </html>
