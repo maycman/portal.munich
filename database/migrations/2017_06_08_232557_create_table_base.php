41a9d01a048b4bdc09e7466353c735c6a7859afd
Michael <pandamayc_20@hotmail.com>
2017-07-21 11:45:25 -0500

    Cargador de Archivos csv funcional

--- database/migrations/2017_06_08_232557_create_table_base.php
+++ database/migrations/2017_06_08_232557_create_table_base.php
@@ -12,56 +12,56 @@ class CreateTableBase extends Migration
      */
     public function up()
     {
-        Schema::create('registro', function (Blueprint $table) {
+        Schema::create('registros', function (Blueprint $table) {
             $table->increments('id_registro');
-            $table->mediumText('concesionaria');
-            $table->mediumText('empresa');
-            $table->mediumText('razon_social');
-            $table->mediumText('nombre');
-            $table->mediumText('ap_paterno');
-            $table->mediumText('ap_materno');
-            $table->mediumText('sexo');
-            $table->mediumText('estado');
-            $table->mediumText('ciudad');
-            $table->mediumText('calle');
-            $table->mediumText('num');
-            $table->mediumText('colonia');
-            $table->mediumText('municipio');
-            $table->mediumText('codigo_postal');
-            $table->mediumText('lada');
-            $table->mediumText('telefono1');
-            $table->mediumText('ext1');
-            $table->mediumText('lada_cel');
-            $table->mediumText('lada3');
-            $table->mediumText('celular');
-            $table->mediumText('telefono3');
-            $table->mediumText('lada4');
-            $table->mediumText('telefono4');
-            $table->mediumText('email');
-            $table->mediumText('email2');
-            $table->mediumText('nombre_contacto');
-            $table->mediumText('app_contacto');
-            $table->mediumText('apm_contacto');
-            $table->mediumText('lada_contacto');
-            $table->mediumText('telofono_contacto');
-            $table->mediumText('nombre_modelo');
-            $table->mediumText('modelo');
-            $table->mediumText('chasis');
-            $table->mediumText('placa');
-            $table->mediumText('fecha_entrada');
-            $table->mediumText('fecha_servicio');
-            $table->mediumText('KM');
-            $table->mediumText('tipo_servicio');
-            $table->mediumText('costo');
-            $table->mediumText('no_orden');
-            $table->mediumText('fecha_insercion');
-            $table->mediumText('ano_modelo');
-            $table->mediumText('nombre_asesor');
-            $table->mediumText('app_asesor');
-            $table->mediumText('apm_asesor');
-            $table->mediumText('tecnico');
-            $table->mediumText('contactable');
-            $table->mediumText('cache');
+            $table->string('concesionaria');
+            $table->string('empresa')->nullable();
+            $table->string('razon_social')->nullable();
+            $table->string('nombre')->nullable();
+            $table->string('ap_paterno')->nullable();
+            $table->string('ap_materno')->nullable();
+            $table->string('sexo')->nullable();
+            $table->string('estado')->nullable();
+            $table->string('ciudad')->nullable();
+            $table->string('calle')->nullable();
+            $table->string('num')->nullable();
+            $table->string('colonia')->nullable();
+            $table->string('municipio')->nullable();
+            $table->string('codigo_postal')->nullable();
+            $table->string('lada')->nullable();
+            $table->string('telefono1')->nullable();
+            $table->string('ext1')->nullable();
+            $table->string('lada_cel')->nullable();
+            $table->string('lada3')->nullable();
+            $table->string('celular')->nullable();
+            $table->string('telefono3')->nullable();
+            $table->string('lada4')->nullable();
+            $table->string('telefono4')->nullable();
+            $table->string('email')->nullable();
+            $table->string('email2')->nullable();
+            $table->string('nombre_contacto')->nullable();
+            $table->string('app_contacto')->nullable();
+            $table->string('apm_contacto')->nullable();
+            $table->string('lada_contacto')->nullable();
+            $table->string('telofono_contacto')->nullable();
+            $table->string('nombre_modelo')->nullable();
+            $table->string('modelo');
+            $table->string('chasis');
+            $table->string('placa')->nullable();
+            $table->string('fecha_entrada');
+            $table->string('fecha_servicio');
+            $table->string('KM');
+            $table->string('tipo_servicio');
+            $table->string('costo')->nullable();
+            $table->string('no_orden');
+            $table->string('fecha_insercion');
+            $table->string('ano_modelo')->nullable();
+            $table->string('nombre_asesor');
+            $table->string('app_asesor');
+            $table->string('apm_asesor');
+            $table->string('tecnico')->nullable();
+            $table->string('contactable');
+            $table->string('cache')->nullable();
             $table->timestamps();
         });
     }
@@ -73,6 +73,6 @@ class CreateTableBase extends Migration
      */
     public function down()
     {
-        Schema::drop('registro');
+        Schema::drop('registros');
     }
 }
